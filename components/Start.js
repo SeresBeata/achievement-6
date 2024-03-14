3870ff753f7c367d6ed05c8042728d13ae955d9b
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-03-14 14:18:33 +0100

    Create Start component
    Create and export Start child component. Import components from React Native. Create simple style for Start component.

---
+++ components/Start.js
@@ -0,0 +1,46 @@
+// import components from React Native
+import { StyleSheet, View, Text, TextInput, Button } from 'react-native';
+
+const Start = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.titleContainer}>
+        <Text>App Title</Text>
+      </View>
+      <View>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Type your username here"
+        ></TextInput>
+        <Button title="Start Chatting" />
+      </View>
+    </View>
+  );
+};
+
+//Create style
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  titleContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  settingsContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textInput: {
+    width: '88%',
+    padding: 15,
+    borderWidth: 1,
+    marginTop: 15,
+    marginBottom: 15,
+  },
+});
+
+export default Start;
