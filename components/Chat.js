28a30bfdce05f0d6a7adf706e749c93ce67f46dc
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-03-14 19:58:28 +0100

    Extract prop at Chat.js
    Extract the route prop in the Chat component's function definition. Extract the name route parameter passed from Start.

--- components/Chat.js
+++ components/Chat.js
@@ -1,10 +1,14 @@
 // import components from React Native
 import { StyleSheet, View, Text } from 'react-native';
 
-const Chat = () => {
+const Chat = ({ route }) => {
+  //extract the name route parameter passed from Start
+  const { name } = route.params;
+
   return (
     <View style={styles.container}>
       <Text> This is the Chat!</Text>
+      <Text> Username: {name}</Text>
     </View>
   );
 };
