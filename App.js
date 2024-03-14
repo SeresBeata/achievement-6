a0df783ccf29a66b5fc869b55fd9b17af05f5cf3
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-03-14 14:57:26 +0100

    Update App.js for navigation
    Import NavigationContainer and createNativeStackNavigator from React Navigation library. Create the navigator with createNativeStackNavigator(). Use  <NavigationContainer>. Use the Navigator component from the Stack object (e.g., Stack.Navigator), along with the Screen component from the same object (e.g., Stack.Screen).

--- App.js
+++ App.js
@@ -4,8 +4,22 @@ import { StyleSheet, View } from 'react-native';
 import Start from './components/Start';
 import Chat from './components/Chat';
 
+// import React Navigation library
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// Create the navigator
+const Stack = createNativeStackNavigator();
+
 const App = () => {
-  return <View style={styles.container}></View>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Start">
+        <Stack.Screen name="Start" component={Start} />
+        <Stack.Screen name="Chat" component={Chat} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 const styles = StyleSheet.create({
