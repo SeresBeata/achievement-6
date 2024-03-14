983a06e42fdbc66d93a8241a42c3f4c85f5e913f
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-03-14 15:38:23 +0100

    Delete style from App.js
    Delete unnecessary style from App.js file.

--- App.js
+++ App.js
@@ -22,13 +22,4 @@ const App = () => {
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
 export default App;
