f5239b2b3f398a376b3231044a5cc603f9c89046
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-03-15 11:36:37 +0100

    Update Color Picker Section
    Create state variables with initial state "false" for every color option. Use onPress to call setBackground() setter function and setPickedColor setter functions. Set state variable to true on the picked color option, and set false on the other remaining option. Add <View> elements around <TouchableOpacity> elements and style it if the color option has been picked. Use ternary operator to change the style of <View> element of color option if pickedcolor is true.

--- components/Start.js
+++ components/Start.js
@@ -23,6 +23,12 @@ const Start = ({ navigation }) => {
   //create state variable  with initial state ''
   const [background, setBackground] = useState('');
 
+  //create state varables for picked color of color picker
+  const [pickedcolor1, setPickedcolor1] = useState(false);
+  const [pickedcolor2, setPickedcolor2] = useState(false);
+  const [pickedcolor3, setPickedcolor3] = useState(false);
+  const [pickedcolor4, setPickedcolor4] = useState(false);
+
   return (
     <View style={styles.container}>
       <ImageBackground source={bgImg} resizeMode="cover" style={styles.bgImg}>
@@ -44,22 +50,72 @@ const Start = ({ navigation }) => {
           <View style={{ width: '88%' }}>
             <Text style={styles.colorText}>Choose Background Color:</Text>
             <View style={styles.colorContainer}>
-              <TouchableOpacity
-                style={[styles.colorTemplate, styles.colorOne]}
-                onPress={() => setBackground(styles.colorOne.backgroundColor)}
-              ></TouchableOpacity>
-              <TouchableOpacity
-                style={[styles.colorTemplate, styles.colorTwo]}
-                onPress={() => setBackground(styles.colorTwo.backgroundColor)}
-              ></TouchableOpacity>
-              <TouchableOpacity
-                style={[styles.colorTemplate, styles.colorThree]}
-                onPress={() => setBackground(styles.colorThree.backgroundColor)}
-              ></TouchableOpacity>
-              <TouchableOpacity
-                style={[styles.colorTemplate, styles.colorFour]}
-                onPress={() => setBackground(styles.colorFour.backgroundColor)}
-              ></TouchableOpacity>
+              <View
+                // use ternary operator and style the View component if pickedcolor1 is true
+                style={pickedcolor1 ? styles.pickedColor : styles.noPickedColor}
+              >
+                <TouchableOpacity
+                  style={[styles.colorTemplate, styles.colorOne]}
+                  // onPress={() => setBackground(styles.colorOne.backgroundColor)}
+                  onPress={() => {
+                    setBackground(styles.colorOne.backgroundColor);
+                    setPickedcolor1(true);
+                    setPickedcolor2(false);
+                    setPickedcolor3(false);
+                    setPickedcolor4(false);
+                  }}
+                ></TouchableOpacity>
+              </View>
+              <View
+                // use ternary operator and style the View component if pickedcolor2 is true
+                style={pickedcolor2 ? styles.pickedColor : styles.noPickedColor}
+              >
+                <TouchableOpacity
+                  style={[styles.colorTemplate, styles.colorTwo]}
+                  // onPress={() => setBackground(styles.colorTwo.backgroundColor)}
+                  onPress={() => {
+                    setBackground(styles.colorTwo.backgroundColor);
+                    setPickedcolor1(false);
+                    setPickedcolor2(true);
+                    setPickedcolor3(false);
+                    setPickedcolor4(false);
+                  }}
+                ></TouchableOpacity>
+              </View>
+              <View
+                // use ternary operator and style the View component if pickedcolor3 is true
+                style={pickedcolor3 ? styles.pickedColor : styles.noPickedColor}
+              >
+                <TouchableOpacity
+                  style={[styles.colorTemplate, styles.colorThree]}
+                  // onPress={() =>
+                  //   setBackground(styles.colorThree.backgroundColor)
+                  // }
+                  onPress={() => {
+                    setBackground(styles.colorThree.backgroundColor);
+                    setPickedcolor1(false);
+                    setPickedcolor2(false);
+                    setPickedcolor3(true);
+                    setPickedcolor4(false);
+                  }}
+                ></TouchableOpacity>
+              </View>
+              <View
+                // use ternary operator and style the View component if pickedcolor4 is true
+                style={pickedcolor4 ? styles.pickedColor : styles.noPickedColor}
+              >
+                <TouchableOpacity
+                  style={[styles.colorTemplate, styles.colorFour]}
+                  // onPress={() => setBackground(styles.colorFour.backgroundColor)}
+                  onPress={() => {
+                    setBackground(styles.colorFour.backgroundColor);
+                    setPickedcolor1(false);
+                    setPickedcolor2(false);
+                    setPickedcolor3(false);
+                    setPickedcolor4(true);
+                  }}
+                ></TouchableOpacity>
+              </View>
             </View>
           </View>
           {/* End: Color Picker Section  */}
@@ -182,6 +238,18 @@ const styles = StyleSheet.create({
     fontSize: 17,
     fontWeight: '600',
     color: 'white',
+  },
+  pickedColor: {
+    padding: 2,
+    borderWidth: 2,
+    borderColor: '#757083',
+    borderRadius: 50,
+  },
+  noPickedColor: {
+    padding: 2,
+    borderWidth: 2,
+    borderColor: 'white',
+    borderRadius: 50,
   },
 });
 
