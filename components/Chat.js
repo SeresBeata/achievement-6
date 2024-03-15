8a29f37627579495b6d9e6a5d4f13bb8db3deec6
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-03-15 15:33:22 +0100

    Uodate Chat.js with GiftedChat
    Import useState, useEffect and GiftedChat. Create state variable called "messages" with initial state empty array. Add the useEffect() function to set the state of "message" with a static message by using the setter method of the messages state. Create custom function onSend() and use append() function provided by GiftedChat to append the new message. Use GiftedChat component. Delete previous components.

--- components/Chat.js
+++ components/Chat.js
@@ -1,3 +1,8 @@
+import { useState, useEffect } from 'react';
+
+//import the Gifted Chat library
+import { GiftedChat } from 'react-native-gifted-chat';
+
 // import components from React Native
 import { StyleSheet, View, Text } from 'react-native';
 
@@ -7,11 +12,44 @@ const Chat = ({ route }) => {
   //extract the background route parameter passed from Start
   const { background } = route.params;
 
+  //create state variable with initial state empty array
+  const [messages, setMessages] = useState([]);
+
+  //use useEffect for messages
+  //Messages must follow a certain format to work with the Gifted Chat library.
+  //each message requires an ID, a creation date, and a user object.
+  //user object requires a user ID, name, and avatar.
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+    ]);
+  }, []);
+
+  //create custom function
+  const onSend = (newMessages) => {
+    setMessages((previousMessages) =>
+      //use append() function provided by GiftedChat, which appends the new message
+      GiftedChat.append(previousMessages, newMessages)
+    );
+  };
+
   return (
-    <View style={[styles.container, { backgroundColor: background }]}>
-      <Text style={[styles.text, styles.Title]}> This is the Chat!</Text>
-      <Text style={styles.text}> Username: {name}</Text>
-    </View>
+    <GiftedChat
+      messages={messages}
+      onSend={(messages) => onSend(messages)}
+      user={{
+        _id: 1,
+      }}
+    />
   );
 };
 
