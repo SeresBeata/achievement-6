e7ab5d701adc46077b8e39723be2284634528142
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-03-19 08:41:40 +0100

    Initialize connection to Firestore
    Import initializeApp() and getFirestore(). Add Firebase configuration. Initialize Firebase and Cloud Firestore.

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
 import { StyleSheet, View } from 'react-native';
+//To initialize a connection for Firestore import initializeApp() and getFirestore()
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
 //import child components
 import Start from './components/Start';
@@ -12,6 +15,22 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+  //Firebase configuration
+  const firebaseConfig = {
+    apiKey: 'AIzaSyCfO9_Qph_muGtU8U4IgR-hOzdtyAMcnhA',
+    authDomain: 'chatapp-b1fe8.firebaseapp.com',
+    projectId: 'chatapp-b1fe8',
+    storageBucket: 'chatapp-b1fe8.appspot.com',
+    messagingSenderId: '307748943604',
+    appId: '1:307748943604:web:13a28f428490e6f15e50a1',
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  // Initialize Cloud Firestore and get a reference to the service
+  const db = getFirestore(app);
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Start">
