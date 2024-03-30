74f5594ebb37315a4fa67bde4b9f5e8acd276153
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-03-30 18:51:03 +0100

    Update CustomActions with useActionSheet
    Import useActionSheet. Create function called onActionPress to display action menu "ActionSheet" that contains options. Determine the position of cancel button of ActionSheet, so that ActionSheet can close the view if the user presses "Cancel". Use showActionSheetWithOptions() function to initialize and show the ActionSheet. Call the onActionPress function in case of btn press.

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -1,10 +1,51 @@
 //import components from React Native
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
+//import useActionSheet
+//do not need to install @expo/react-native-action-sheet because it is already installed with react-native-gifted-chat module
+import { useActionSheet } from '@expo/react-native-action-sheet';
+
 //create child component
 const CustomActions = ({ wrapperStyle, iconTextStyle }) => {
+  //use useActionSheet()
+  const actionSheet = useActionSheet();
+
+  //create function to display action menu "ActionSheet" that contains options
+  const onActionPress = () => {
+    //define an array of strings to display in the ActionSheet
+    const options = [
+      'Choose From Library',
+      'Take Picture',
+      'Send Location',
+      'Cancel',
+    ];
+    //determine the position of cancel button of ActionSheet, so that ActionSheet can close the view if the user presses "Cancel"
+    const cancelButtonIndex = options.length - 1;
+
+    //use showActionSheetWithOptions() function to initialize and show the ActionSheet
+    actionSheet.showActionSheetWithOptions(
+      {
+        options,
+        cancelButtonIndex,
+      },
+      async (buttonIndex) => {
+        switch (buttonIndex) {
+          case 0:
+            pickImage();
+            return;
+          case 1:
+            takePhoto();
+            return;
+          case 2:
+            getLocation();
+          default:
+        }
+      }
+    );
+  };
+
   return (
-    <TouchableOpacity style={styles.container}>
+    <TouchableOpacity style={styles.container} onPress={onActionPress}>
       <View style={[styles.wrapper, wrapperStyle]}>
         <Text style={[styles.iconText, iconTextStyle]}>+</Text>
       </View>
